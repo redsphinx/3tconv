1f277ccc2f3183314bf5b053a312709e267dfd0b
redsphinx <gabi.ras@hotmail.com>
2020-10-26 17:36:11 +0100

    tried out toy dataset with circles instead of lines

--- data/create_toy_dataset.py
+++ data/create_toy_dataset.py
@@ -1,7 +1,8 @@
 # This is a toy dataset with 3 classes so that we can validate 3TConv
 import numpy as np
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
+import tkinter as tk
 import time
 from datetime import datetime
 import skvideo.io as skvid
@@ -51,6 +52,147 @@ def generate_sin_wave(amplitude, phase, frequency, rotate=None, height=32, width
     result = U.normalize_between(result, result.min(), result.max(), 0, 255)
 
     return result
+
+
+def generate_dots(the_class, direction=None, speed=None, num_frames=30, window_side=32):
+
+    '''
+    depending on the class, generate an image and then slide over it with a window
+    total canvas size
+    translate: maxspeed * frames * 2
+    rotate: sqrt(23^2+32^2)
+    scale:
+
+    '''
+
+    if the_class == 'translate':
+        if speed is None:
+            speed = 1
+        full_side = window_side + speed * num_frames * 2
+
+    elif the_class == 'rotate':
+
+        full_side = int(np.sqrt(2 * window_side**2)) + 1
+
+    elif the_class == 'scale':
+        if speed is None:
+            speed = 1
+        full_side = window_side + speed * num_frames * 2
+
+
+    else:
+        print('error: class not recognized')
+        return None
+
+    # canvas
+    image = Image.new('RGB', (full_side, full_side))
+    canvas = ImageDraw.Draw(image)
+
+    # dots
+    num_dots = np.random.randint(5, 40)
+    radius_dot_min = 2
+    radius_dot_max = 5
+
+    if the_class == 'scale':
+        if direction == 1:
+            radius_dot_min = 1
+            radius_dot_max = 4
+        else:
+            radius_dot_min = 3
+            radius_dot_max = 6
+
+    sample_x = np.random.randint(5, full_side-6, num_dots)
+    sample_y = np.random.randint(5, full_side-6, num_dots)
+    sizes_dots = np.random.randint(radius_dot_min, radius_dot_max, num_dots)
+
+    # draw on canvas
+    for n in range(num_dots):
+        canvas.ellipse((sample_x[n], sample_y[n], sample_x[n]+sizes_dots[n], sample_y[n]+sizes_dots[n]), fill='white')
+
+    # tmp_path = os.path.join(PP.gaff_samples, "%s_dots_%d.jpg" % (the_class, np.random.randint(1000, 9999)))
+    # image.save(tmp_path)
+
+    return image, canvas, full_side
+
+
+def generate_sequence_dots(the_class, num_frames, seed, window_side):
+
+    if the_class == 'translate':
+        direction_hor = np.random.randint(-1, 2)
+        direction_ver = np.random.randint(-1, 2)
+        speed = np.random.randint(1, 4)
+        direction = None
+    elif the_class == 'rotate':
+        direction = np.random.randint(0, 2)
+        if direction == 0:
+            direction = -1
+        speed = np.random.randint(1, 4)
+    elif the_class == 'scale':
+        direction = np.random.randint(0, 2)
+        if direction == 0:
+            direction = -1
+        speed = np.random.randint(1, 3)
+    else:
+        print('error: class not recognized')
+
+    image, canvas, full_side = generate_dots(the_class, direction, speed, num_frames)
+
+    if the_class == 'translate':
+        left_x = full_side // 2 - window_side // 2
+        up_y = full_side // 2 - window_side // 2
+
+        for f in range(num_frames):
+            bbox_left = f * speed * direction_hor + left_x
+            bbox_right = bbox_left + window_side
+            bbox_up = f * speed * direction_ver + up_y
+            bbox_down = bbox_up + window_side
+
+            frame = image.crop((bbox_left, bbox_up, bbox_right, bbox_down))
+            save_path = os.path.join(PP.gaff_samples, "%s_2_frame_%d.jpg" % (the_class, f))
+            frame.save(save_path)
+
+    elif the_class == 'rotate':
+        bbox_left = full_side // 2 - window_side // 2
+        bbox_up = full_side // 2 - window_side // 2
+        bbox_right = bbox_left + window_side
+        bbox_down = bbox_up + window_side
+
+        for f in range(num_frames):
+            image_rot = image.rotate(f * speed * direction, resample=Image.BILINEAR)
+            frame = image_rot.crop((bbox_left, bbox_up, bbox_right, bbox_down))
+            save_path = os.path.join(PP.gaff_samples, "%s_2_frame_%d.jpg" % (the_class, f))
+            frame.save(save_path)
+
+    elif the_class == 'scale':
+        if direction == 1: # zoom in, things get bigger
+            left_x = 0
+            up_y = 0
+            right_x = full_side
+            down_y = full_side
+        else: # zoom out, things get smaller
+            left_x = full_side // 2 - window_side // 2
+            up_y = full_side // 2 - window_side // 2
+            right_x = left_x + window_side
+            down_y = up_y + window_side
+
+        for f in range(num_frames):
+            bbox_left = left_x + f * direction * speed
+            bbox_right = right_x - f * direction * speed
+            bbox_up = up_y + f * direction * speed
+            bbox_down = down_y - f * direction * speed
+
+            frame = image.crop((bbox_left, bbox_up, bbox_right, bbox_down))
+            frame = frame.resize((window_side, window_side), Image.BILINEAR)
+
+            save_path = os.path.join(PP.gaff_samples, "%s_2_frame_%d.jpg" % (the_class, f))
+            frame.save(save_path)
+
+        # get coordinates of bounding box
+        # crop, resize and save
+        pass
+
+
+generate_sequence_dots(the_class='scale', num_frames=30, seed=0, window_side=32)
 
 
 def rotate_grid(grid, degrees):
@@ -283,13 +425,13 @@ def generate_single_sequence(the_class, height, width, frames, seed, randomize=T
 # make_sample(amplitude=amplitude, phase=phase, frequency=frequency, rotation=10)
 
 
-the_class = 'translate'
-# the_class = 'rotate'
-# the_class = 'scale'
-height, width = 32, 32
-frames = 30
-seed = 420
-generate_single_sequence(the_class, height, width, frames, seed, randomize=False)
+# the_class = 'translate'
+# # the_class = 'rotate'
+# # the_class = 'scale'
+# height, width = 32, 32
+# frames = 30
+# seed = 420
+# generate_single_sequence(the_class, height, width, frames, seed, randomize=False)
 
 '''
 SEEDS
